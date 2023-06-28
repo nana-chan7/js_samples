ddafab9a586c7798f7b76ec3e74eef25755c180b
{marimo_7} <{220098tt@yse-c.net}>
2023-06-28 12:16:22 +0900

    update

--- js/function.js
+++ js/function.js
@@ -5,3 +5,30 @@ function calculateTotalPrice(price, amount) { // function statment
 
 var totalPrice = calculateTotalPrice(200, 5);
 console.log(totalPrice);
+
+
+// 無名関数 (anonymous function)
+const hello = function(name){
+    var message = name + "さん、こんにちは！";
+    return message;
+}
+
+var message = hello("YSE");
+console.log(message);
+
+
+// アロー関数 (arrow function)
+const hello2 = (name) => {
+    var message = name + "さん、こんにちは！";
+    return message;
+}
+
+var message = hello2("東京 太郎");
+console.log(message);
+
+// html がすべて読み込まれてから実行される(誤動作がなくなる？)
+window.onload = function(){
+    console.log("東京");
+}
+
+console.log("横浜");
