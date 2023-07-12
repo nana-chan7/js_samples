cde31e1115441736a7aec9dbfe2a797f0988d250
{marimo_7} <{220098tt@yse-c.net}>
2023-07-12 11:24:48 +0900

    Switch

--- js/condition.js
+++ js/condition.js
@@ -1,5 +1,6 @@
 // 2023-07-12 2.week5
 
+// If-Else
 var message = ""
 var price = 200
 var money = 500
@@ -13,3 +14,21 @@ if (price <= 0) { // If-Else Statement
 }
 console.log(message)
 
+
+// Switch
+var weekday = "水"
+var garbage = ""
+
+switch (weekday) { // Switch Statement
+    case "月":
+    case "金":
+        garbage = "燃えるゴミ"
+        break;
+    case "水":
+        garbage = "燃えないゴミ"
+        break;
+    default:
+        garbage = "回収なし"
+        break;
+}
+document.getElementById("garbage").innerHTML = garbage
