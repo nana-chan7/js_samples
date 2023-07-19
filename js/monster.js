a1c89d2f95c7cf626ace6824ddcfc7da490d7010
{marimo_7} <{220098tt@yse-c.net}>
2023-07-19 10:17:20 +0900

    画像追加

--- js/monster.js
+++ js/monster.js
@@ -4,8 +4,23 @@
 var stage = document.getElementById("stage")
 
 // child (子要素)
-var monsterElement = document.createElement("p") // 文字列でタグを指定
-monsterElement.innerHTML = "スライム"
+var monsterElement1 = document.createElement("p") // 文字列でタグを指定
+monsterElement1.innerHTML = "スライム"
 
 // parent に追加
-stage.appendChild(monsterElement)
+stage.appendChild(monsterElement1)
+
+
+var monsterElement2 = document.createElement("p") 
+monsterElement2.innerHTML = "キメラ"
+stage.appendChild(monsterElement2)
+
+// child の削除
+stage.removeChild(monsterElement1)
+
+// aリンク追加
+var a = document.createElement("a")
+a.href = "https://www.google.com/search?q=" + monsterElement1.innerHTML
+a.target = "_blank" // 新しいブラウザで開く
+a.innerHTML = monsterElement1.innerHTML
+stage.appendChild(a)
