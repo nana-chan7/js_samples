c90361db2d6571796f631cb101887ef0a3b1cd55
{marimo_7} <{220098tt@yse-c.net}>
2023-07-19 11:23:53 +0900

    多次元配列 & オブジェクト

--- js/array_multi.js
+++ js/array_multi.js
@@ -20,9 +20,29 @@ console.log(character)
 
 // 多次元配列
 var rows = [
-    [90, 78, 82,],
-    [62, 70, 68,],
-    [68, 88, 72,],
+   // 0,  1,  2, ...
+    [90, 78, 82,], // 0
+    [62, 70, 68,], // 1
+    [68, 88, 72,], // 2
 ]
-
 console.log(rows[1][1])
+console.log(rows[2][1])
+
+// 多次元配列 & オブジェクト
+var users=[
+    {
+        id: 1,
+        name: "User1",
+        email: "user1@test.com", 
+    },
+    {
+        id: 2,
+        name: "User2",
+        email: "user2@test.com", 
+    },
+    {
+        id: 3,
+        name: "User3",
+        email: "user3@test.com", 
+    },
+]
