d9029d5ab012a8e35bec145e59d5d963eb5a958d
{marimo_7} <{220098tt@yse-c.net}>
2023-07-19 09:57:16 +0900

    element 作成

--- js/monster.js
+++ js/monster.js
@@ -1,3 +1,11 @@
 // 2023-07-19 2.week6
 
+// parent (親要素)
 var stage = document.getElementById("stage")
+
+// child (子要素)
+var monsterElement = document.createElement("p") // 文字列でタグを指定
+monsterElement.innerHTML = "スライム"
+
+// parent に追加
+stage.appendChild(monsterElement)
