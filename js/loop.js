07666cd92ccbdbf5c074f84db94db123a6433f4d
{marimo_7} <{220098tt@yse-c.net}>
2023-08-30 10:32:37 +0900

    loop処理

---
+++ js/loop.js
@@ -0,0 +1,34 @@
+// 2023-08-30 2.week7
+
+const person = {
+    name: "Alice",
+    age: 30,
+    occupation: "Enginner",
+}
+
+// for-in
+console.log("--- for-in ---")
+for (const key in person) {
+    // ※ ↓ 自動入力されたものを編集した
+    if (person.hasOwnProperty(key)) 
+        {
+            const value = person[key];
+            console.log(value)
+        }
+}
+
+
+const drinks = ["コーヒー", "紅茶", "ほうじ茶"]
+
+// for-of
+console.log("--- for-of ---")
+for (const value of drinks) {
+    console.log(value)
+}
+
+// fori
+console.log("--- for ---")
+for (var index =0; index <drinks.length; index++){
+    const drink = drinks[index];
+    console.log(drink)
+}
