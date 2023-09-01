5c485b0c629678559d77635e56f9ef3a483cfd90
{marimo_7} <{220098tt@yse-c.net}>
2023-09-01 14:08:00 +0900

    classList

--- js/style.js
+++ js/style.js
@@ -1,5 +1,18 @@
-const messageElement = document.getElementById("message")
+const messageElement = document.getElementById('message')
 
 function add(){
-    messageElement.classList.add("active")
+    messageElement.classList.add('active')
+
+}
+
+function remove(){
+    messageElement.classList.remove('active')
+}
+
+function toggle(){
+    messageElement.classList.toggle('active')
+}
+
+function replace(){
+    messageElement.classList.replace('massege', 'frame')
 }
