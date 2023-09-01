0d1f1aeaa63247c1679c6051c5842a63d871bd99
{marimo_7} <{220098tt@yse-c.net}>
2023-09-01 14:35:43 +0900

    thisオブジェクト

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,38 @@
 console.log(this)
+
+const messageElement = document.getElementById('message')
+
+this.name = "Bob"
+
+function sayFunction(element){
+    console.log(element)
+    messageElement.innerHTML = element.name
+}
+
+const person = {
+    name: "Jhon",
+    sayHello1: function(){
+        messageElement.innerHTML = this.name
+    },
+
+    sayHello2: () =>{
+        messageElement.innerHTML = this.name
+    },
+}
+
+function sayAnonymous(element){
+    person.sayHello1()
+}
+
+function sayArrow(element){
+    person.sayHello2()
+}
+
+function Person(name){
+    this.name = name
+    messageElement.innerHTML = this.name
+}
+
+function sayPerson(element){
+    new Person("Alice")
+}
