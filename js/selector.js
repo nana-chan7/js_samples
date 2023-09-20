fc03264beb5d32edf49318d07344cf7934e1bf15
{marimo_7} <{220098tt@yse-c.net}>
2023-09-20 09:44:46 +0900

    querySelector

---
+++ js/selector.js
@@ -0,0 +1,38 @@
+// 2023-09-20 3.week1
+
+// ※ 2つある h2タグの１つだけ←初めに見つかったやつ
+console.log("--- querySelector ---")
+var h2 = document.querySelector('h2') 
+console.log(h2)
+
+// All ← 複数
+console.log("--- querySelectorAll ---")
+var h2 = document.querySelectorAll('h2')
+// var h2 = document.getElementsByTagName('h2')  // ※ 同じ処理
+console.log(h2[0]) // インデックスを指定する
+console.log(h2[1])
+
+// id を指定
+console.log("--- id(#)で指定 ---")
+var cityTitle = document.querySelector('#city-title')
+// var cityTitle = document.getElementById('city-title')  // ※ 同じ処理
+console.log(cityTitle)
+
+// 表示される型が違う
+console.log("-----------")
+var cities = document.getElementsByClassName('city')
+console.log(cities)
+var cities = document.querySelectorAll('.city') 
+console.log(cities)
+
+for (const city of cities) {
+    console.log(city.innerHTML)
+}
+
+console.log("-----------")
+var items = document.querySelectorAll('ul.menus > li')
+console.log(items)
+// 'ul.menus > li' ulタグmenusクラスの子(>: 親子関係) li
+
+// jQuery だと↓
+// $('ul.menus > li)
